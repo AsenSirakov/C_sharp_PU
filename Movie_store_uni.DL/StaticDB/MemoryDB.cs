4999f90be760a7cf4ae3b9ca701ff2d4a269df75
Asen Sirakov <asko2040@abv.bg>
2024-11-04 19:11:30 +0200

    Project_Uni

---
+++ Movie_store_uni.DL/StaticDB/MemoryDB.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Movie_store_uni.ML.DTOS;
+
+namespace Movie_store_uni.DL.StaticDB
+{
+    internal static class MemoryDB
+    {
+      public static List<Movie> Movies = new List<Movie>
+    }
+}

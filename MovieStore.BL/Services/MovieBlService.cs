800e4d59d9d1219d8c2a109cb4a3c062173ec3bd
Asen Sirakov <asko2040@abv.bg>
2024-12-02 18:27:34 +0200

    Commit

---
+++ MovieStore.BL/Services/MovieBlService.cs
@@ -0,0 +1,17 @@
+﻿using MovieStore.Models.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieStore.BL.Services
+{
+    internal class MovieBlService : 
+    {
+        public IEnumerable<MoviesView> GetDetailedMovies()
+        {
+
+        }
+    }
+}

97066524c798ffaee2367b87d2c5e37a84e5cc33
Asen Sirakov <asko2040@abv.bg>
2025-02-28 23:11:18 +0200

    Asynch programming examples

---
+++ Secon_semester/ConsoleApp1/Program.cs
@@ -0,0 +1,39 @@
+﻿internal class Program
+{
+    static int Count = 0;
+    static void Main(string[] args)
+    {
+        Task task1 = Task.Run(() =>
+        {
+           
+            PrintCounter();
+        });
+     
+
+        Task task2 = Task.Run(() =>
+        {
+            
+            PrintCounter();
+        });
+        task2.Wait();
+
+        Task task3 = Task.Run(() =>
+        {
+            PrintCounter();
+        });
+        
+        Task.WaitAll(task1, task2, task3);
+    }
+
+    static void PrintCounter()
+    {
+        int count = 0;
+
+        for (int i = 0; i < 10000; i++)
+        {
+            Count++;
+        }
+        Console.WriteLine(Count);
+    }
+
+}

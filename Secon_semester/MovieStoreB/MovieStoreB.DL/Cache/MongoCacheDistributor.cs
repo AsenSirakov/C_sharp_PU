3efd7497eefad1095d95ac0448f9ee09d5065bcd
Asen Sirakov <asko2040@abv.bg>
2025-05-15 22:30:10 +0300

    cc

--- Secon_semester/MovieStoreB/MovieStoreB.DL/Cache/MongoCacheDistributor.cs
+++ Secon_semester/MovieStoreB/MovieStoreB.DL/Cache/MongoCacheDistributor.cs
@@ -1,21 +1,26 @@
 ﻿using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Options;
+using MovieStoreB.DL.Kafka;
+using MovieStoreB.Models.Configurations.CachePopulator;
 using MovieStoreB.Models.DTO;
 
 namespace MovieStoreB.DL.Cache
 {
-    public class MongoCachePopulator<TData, TDataRepository, TConfigurationType, TKey> : BackgroundService 
-        where TDataRepository : ICacheRepository<TData>
-        where TData : CacheItem<TKey>
+    public class MongoCachePopulator<TData, TConfigurationType, TKey> : BackgroundService 
+        //where TDataRepository : ICacheRepository<TKey, TData>
+        where TKey : notnull
+        where TData : ICacheItem<TKey>
         where TConfigurationType : CacheConfiguration
     {
-        private readonly ICacheRepository<TData> _cacheRepository;
+        private readonly ICacheRepository<TKey, TData> _cacheRepository;
         private readonly IOptionsMonitor<TConfigurationType> _configuration;
+        private readonly IKafkaProducer<TData> _kafkaProducer;
 
-        public MongoCachePopulator(ICacheRepository<TData> cacheRepository, IOptionsMonitor<TConfigurationType> configuration)
+        public MongoCachePopulator(ICacheRepository<TKey, TData> cacheRepository, IOptionsMonitor<TConfigurationType> configuration, IKafkaProducer<TData> kafkaProducer)
         {
             _cacheRepository = cacheRepository;
             _configuration = configuration;
+            _kafkaProducer = kafkaProducer;
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
@@ -23,6 +28,11 @@ namespace MovieStoreB.DL.Cache
             var lastExecuted = DateTime.UtcNow;
 
             var result = await _cacheRepository.FullLoad();
+
+            if (result != null && result.Any())
+            {
+                await _kafkaProducer.ProduceAll(result);
+            }
 
             while (!stoppingToken.IsCancellationRequested)
             {
@@ -34,6 +44,8 @@ namespace MovieStoreB.DL.Cache
                 {
                     continue;
                 }
+
+                await _kafkaProducer.ProduceAll(updatedData);
 
                 var lastUpdated = updatedData.Last()?.DateInserted;
 

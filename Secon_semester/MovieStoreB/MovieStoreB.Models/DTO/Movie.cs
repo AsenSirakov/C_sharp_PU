df7237cb799039f28f6a4a55ee01c336fbf91564
Asen Sirakov <asko2040@abv.bg>
2025-06-04 18:51:55 +0200

    Commit

--- Secon_semester/MovieStoreB/MovieStoreB.Models/DTO/Movie.cs
+++ Secon_semester/MovieStoreB/MovieStoreB.Models/DTO/Movie.cs
@@ -6,8 +6,7 @@ namespace MovieStoreB.Models.DTO
     public record Movie : ICacheItem<string>
     {
         [Key(0)]
-        public string Id { get; set; }
-
+        public string Id { get; set; }  
 
         [Key(1)]
         public string Title { get; set; }
@@ -19,9 +18,9 @@ namespace MovieStoreB.Models.DTO
         public List<string> ActorIds { get; set; }
 
         [Key(4)]
-        public DateTime DateInserted { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public DateTime DateInserted { get; set; }
 
-        public override string GetKey()
+        public string GetKey()
         {
             return Id;
         }
